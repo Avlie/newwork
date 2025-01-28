7c46b50947b4240b25d8a9caf7453e6eb25902d7
Avlie <antipovicana@gmail.com>
2025-01-28 11:36:10 +0100

    update

--- app/src/main/java/com/example/newwork/MainActivity.java
+++ app/src/main/java/com/example/newwork/MainActivity.java
@@ -27,13 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createnotif();
+        createnotiflong();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
                 requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
             }
 
             Button button = findViewById(R.id.button);
-            button.setOnClickListener(v -> sendNotification());}
+            button.setOnClickListener(v -> sendNotification());
+            Button button1 = findViewById(R.id.button1);
+            button1.setOnClickListener(v -> sendNotificationlong());}
 
     }
     private void createnotif(){
@@ -56,5 +59,26 @@ public class MainActivity extends AppCompatActivity {
         notificationManager.notify(1, builder.build());
 
     }
+    private void createnotiflong(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            CharSequence name = "Kanal powiadomien";
+            String description = "Opis Kanalu powiadonienia";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel chanel = new NotificationChannel(CHANELL_ID, name, importance);
+            chanel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(chanel);
+
+        }
+    }
+    private void sendNotificationlong() {
+        Intent intent =new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANELL_ID).setSmallIcon(R.drawable.g).setContentTitle("nowe").setContentText("ВСЕ ПЛОХА, МИР ВЗАРВЕТСА").setStyle(new NotificationCompat.BigTextStyle().bigText("Lorem2")).setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent).setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+
+    }
+
 
 }
